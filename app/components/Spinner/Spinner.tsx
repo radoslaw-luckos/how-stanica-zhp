6ae433d7fcf459bd6cf76f70fea9d7d0645099a4
radoslaw.luckos <radoslaw.luckos@angrynerds.co>
2023-06-02 23:51:33 +0200

    loading spinner component added

---
+++ app/components/Spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import { MdSailing } from 'react-icons/md';
+
+const Spinner = () => {
+	return (
+		<div className='spinner'>
+			<MdSailing />
+		</div>
+	);
+};
+
+export default Spinner;
---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import Spinner from './components/Spinner/Spinner';
+
+export default function Loading() {
+	// You can add any UI inside Loading, including a Skeleton.
+	return (
+		<main>
+			<Spinner />
+		</main>
+	);
+}
