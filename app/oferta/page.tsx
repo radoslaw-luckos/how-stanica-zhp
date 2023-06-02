522e0e2631cf623b3f144cfc72e093cae4f7e8d5
radoslaw.luckos <radoslaw.luckos@angrynerds.co>
2023-06-02 20:35:06 +0200

    first level routing added

---
+++ app/oferta/page.tsx
@@ -0,0 +1,3 @@
+export default function Offer() {
+	return <main>Oferta</main>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-	return <main>Home Page</main>;
+	return <main>Główna</main>;
 }
